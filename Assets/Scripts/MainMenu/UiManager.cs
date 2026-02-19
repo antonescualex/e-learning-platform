c069f111dbee060f9a452151ac739149d772c13c
Antonescu Alex <alx2004mc@gmail.com>
2026-02-19 12:22:45 +0200

    Redesigned Main Menu

--- Assets/Scripts/MainMenu/UiManager.cs
+++ Assets/Scripts/MainMenu/UiManager.cs
@@ -7,27 +7,31 @@ using UnityEngine.UI;
 public class UiManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text coinsText;
+    
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject optionsMenu;
+    [SerializeField] private GameObject settingsButton;
 
     private void Update()
     {
         UpdateCoinsText();
     }
 
-    public void OnPlayButtonPress()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+    // public void OnPlayButtonPress()
+    // {
+    //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    // }
     
-    public void OnStoreButtonPress()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-    }
+    // public void OnStoreButtonPress()
+    // {
+    //     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    // }
     
 
-    public void OnQuitButtonPress()
-    {
-        Application.Quit();
-    }
+    // public void OnQuitButtonPress()
+    // {
+    //     Application.Quit();
+    // }
 
     private void UpdateCoinsText()
     {
@@ -35,5 +39,12 @@ public class UiManager : MonoBehaviour
         {
             coinsText.text = App.Instance.Profile.Data.Coins.ToString();
         }
+    }
+    
+    public void OnSettingsButtonClick()
+    {
+        mainMenu.SetActive(!mainMenu.activeSelf);
+        optionsMenu.SetActive(!optionsMenu.activeSelf);
+        settingsButton.SetActive(!settingsButton.activeSelf);
     }
 }
