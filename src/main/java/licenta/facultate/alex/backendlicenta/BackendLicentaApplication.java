7875bc4098cb02c9bf468937b39955a0b87aebc5
Antonescu Alex <alx2004mc@gmail.com>
2026-03-27 10:12:38 +0200

    Initial commit

---
+++ src/main/java/licenta/facultate/alex/backendlicenta/BackendLicentaApplication.java
@@ -0,0 +1,13 @@
+package licenta.facultate.alex.backendlicenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLicentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLicentaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/licenta/facultate/alex/backendlicenta/BackendLicentaApplicationTests.java
@@ -0,0 +1,13 @@
+package licenta.facultate.alex.backendlicenta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLicentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
